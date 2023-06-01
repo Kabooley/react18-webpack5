0424589927bd9ad0c759bdd33ea45db659453683
Kabooley <35451559+Kabooley@users.noreply.github.com>
2023-06-01 18:54:17 +0900

    Added resize Handle

--- codesnadbox-test-resizable/test.tsx
+++ codesnadbox-test-resizable/test.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./styles.css";
 import "./react-resizable-essentials.css";
 import { Resizable, ResizableBox } from "react-resizable";
-import type { ResizeCallbackData } from "react-resizable";
+import type { ResizeCallbackData, ResizeHandle } from "react-resizable";
 
 /***
  * resizable wraps...
@@ -14,7 +14,7 @@ import type { ResizeCallbackData } from "react-resizable";
  * 実はResizableBoxにハンドルはちゃんとついている
  * あとはハンドルを右辺全体に変更すること
  *
- * 
+ *
  * TODO: react-resizable-essentials.cssは本当に必要か確認
  * TODO: 辺全体をハンドルにできるようにする
  * */
@@ -50,6 +50,9 @@ export default function App() {
             // Expresses s: south, e: east, n: borth, w: west
             // resizeHandles={["sw", "se", "nw", "ne", "w", "e", "n", "s"]}
             resizeHandles={["e"]}
+            handle={(h, ref) => (
+              <span className={`custom-handle custom-handle-${h}`} ref={ref} />
+            )}
           >
             <div className="pane" style={{ width: paneWidth }}>
               pane
@@ -61,8 +64,14 @@ export default function App() {
             minConstraints={[100, 100]}
             onResize={onEditorSecResize}
             resizeHandles={["e"]}
+            handle={(h, ref) => (
+              <span className={`custom-handle custom-handle-${h}`} ref={ref} />
+            )}
           >
-            <div className="editor-section" style={{width: editorSectionWidth}}>
+            <div
+              className="editor-section"
+              style={{ width: editorSectionWidth }}
+            >
               <div className="monaco-container">
                 <div className="tabs-area">TABS</div>
                 <div className="monaco-editor">EDITOR</div>
