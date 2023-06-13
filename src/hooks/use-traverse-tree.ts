e474ebbd98770674f6589821d226f2def2fb9d99
Kabooley <35451559+Kabooley@users.noreply.github.com>
2023-06-14 00:28:14 +0900

    Update use-traverse-tree.ts

--- src/hooks/use-traverse-tree.ts
+++ src/hooks/use-traverse-tree.ts
@@ -1,7 +1,7 @@
-import type { iExplorer } from '../data/folderData';
+import type { iExplorer } from "../data/folderData";
 
 const useTraverseTree = () => {
-  // 
+  //
   function insertNode(
     tree: iExplorer,
     folderId: string,
@@ -29,32 +29,62 @@ const useTraverseTree = () => {
     return { ...tree, items: latestNode };
   }
 
-  // 
+  //
   // TODO: `explorer.id`だけで削除処理が完了するようにする
-  // 
+  //
   const deleteNode = (
     tree: iExplorer,
-    folderId: string, // 削除されるアイテムが含まれているフォルダのid
     itemId: string, // 削除するアイテムのid
+    isFolder: boolean
   ): iExplorer => {
+    // 削除したいアイテムをitemsに含んでいるか
+    let isItemIncluded: boolean = false;
+    tree.items.forEach((item) => {
+      isItemIncluded = item.id === itemId || isItemIncluded;
+    });
+    
 
-    if (tree.id === folderId) {
+    // 削除したいアイテムをitemsに含んでいるフォルダならば：
+    if (isItemIncluded && isFolder) {
       console.log(`Delete item`);
       return {
         id: tree.id,
         name: tree.name,
         isFolder: tree.isFolder,
-        items: tree.items.filter(item => item.id !== itemId)
+        items: tree.items.filter((item) => item.id !== itemId)
       };
     }
 
     let latestNode: iExplorer[] = [];
     latestNode = tree.items.map((ob) => {
-      return deleteNode(ob, folderId, itemId);
+      return deleteNode(ob, itemId, ob.isFolder);
     });
 
     return { ...tree, items: latestNode };
   };
+
+  // const deleteNode = (
+  //   tree: iExplorer,
+  //   folderId: string, // 削除されるアイテムが含まれているフォルダのid
+  //   itemId: string // 削除するアイテムのid
+  // ): iExplorer => {
+  //   if (tree.id === folderId) {
+  //     console.log(`Delete item`);
+  //     return {
+  //       id: tree.id,
+  //       name: tree.name,
+  //       isFolder: tree.isFolder,
+  //       items: tree.items.filter((item) => item.id !== itemId)
+  //     };
+  //   }
+
+  //   let latestNode: iExplorer[] = [];
+  //   latestNode = tree.items.map((ob) => {
+  //     return deleteNode(ob, folderId, itemId);
+  //   });
+
+  //   return { ...tree, items: latestNode };
+  // };
 
   // NOTE: developing...
   const updateNode = (
@@ -68,8 +98,7 @@ const useTraverseTree = () => {
       // possibly...
       // name of item is changed,
       // dnd to move file,
-    }
-    else if(tree.id === folderId && !tree.isFolder) {
+    } else if (tree.id === folderId && !tree.isFolder) {
       // TODO: mpdify file
     }
 
@@ -77,7 +106,6 @@ const useTraverseTree = () => {
     latestNode = tree.items.map((ob) => {
       return updateNode(ob, folderId, item, isFolder);
     });
-
 
     return { ...tree, items: latestNode };
   };
@@ -87,33 +115,32 @@ const useTraverseTree = () => {
 
 export default useTraverseTree;
 
-
 // -- USAGE --
-// 
+//
 // (function() {
 //   const { insertNode, deleteNode, updateNode } = useTraverseTree();
-//   /*** 
+//   /***
 //    * どのフォルダに、何（フォルダなのかファイルなのか）を追加するのかを指定すれば
 //    * それを追加してくれる
-//    * 
+//    *
 //    * @param {string} folderid - アイテム追加するフォルダのid
 //    * @param {string} item - 追加するアイテムの名前
 //    * @param {boolean} isFolder - フォルダかファイルか
-//    * */ 
+//    * */
 //   const handleInsertNode = (folderId: string, item: string, isFolder: boolean) => {
-//     // @return {iExplorer} finalTree - アイテム追加が反映されたexplorerデータ 
+//     // @return {iExplorer} finalTree - アイテム追加が反映されたexplorerデータ
 //     const finalTree = insertNode(explorer, folderId, item, isFolder);
 //     console.log(finalTree);
 //   };
 
-//   /***** 
+//   /*****
 //    * どのフォルダのどのアイテムなのか、idで指定すれば削除する
-//    * 
+//    *
 //    * @param {string} folderId - 削除するアイテムを所有しているフォルダのid
 //    * @param {string} item - 削除するアイテムの名前
 //    * @param {string} itemId - 削除するアイテムの名前
 //    * @param {boolean} isFolder - 削除するアイテムはフォルダかファイルか   多分要らない。
-//    * */ 
+//    * */
 //   const handleDeleteNode = (folderId: string, itemId: string) => {
 //     const updatedTree = deleteNode(explorer, folderId, itemId);
 //     console.log(updatedTree);
