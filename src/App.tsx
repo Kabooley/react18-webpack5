0e058f8217c1cb30624b280ec68526aa00d305ac
Kabooley <35451559+Kabooley@users.noreply.github.com>
2023-06-14 00:26:58 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,45 @@
-
-/**********************************
- * https://www.youtube.com/watch?v=20F_KzHPpvI
- * 
- * */ 
 import React, { useState } from "react";
 import Folder from "./components/Folder";
 import useTraverseTree from "./hooks/use-traverse-tree";
 import explorer from "./data/folderData";
-import "./index.css";
+import "./styles.css";
 
 export default function App() {
   const [explorerData, setExplorerData] = useState(explorer);
 
   const { insertNode, deleteNode, updateNode } = useTraverseTree();
 
-  const handleInsertNode = (folderId: string, item: string, isFolder: boolean) => {
+  const handleInsertNode = (
+    folderId: string,
+    item: string,
+    isFolder: boolean
+  ) => {
     const finalTree = insertNode(explorerData, folderId, item, isFolder);
     setExplorerData(finalTree);
   };
 
-  /***** 
+  /*****
    * どのフォルダのどのアイテムなのか、idで指定すれば削除する
-   * 
+   *
    * @param {string} folderId - 削除するアイテムを所有しているフォルダのid
    * @param {string} item - 削除するアイテムの名前
    * @param {string} itemId - 削除するアイテムの名前
    * @param {boolean} isFolder - 削除するアイテムはフォルダかファイルか   多分要らない。
-   * */ 
-  const handleDeleteNode = (folderId: string, itemId: string) => {
-    const updatedTree = deleteNode(explorer, folderId, itemId);
+   * */
+
+  const handleDeleteNode = (itemId: string, isFolder: boolean) => {
+    const updatedTree = deleteNode(explorer, itemId, isFolder);
+    setExplorerData(updatedTree);
+    console.log(updatedTree);
   };
 
   return (
     <div className="App">
-      <Folder 
-        handleInsertNode={handleInsertNode} 
-        // handleDeleteNode={handleDeleteNode} 
-        explorer={explorerData} 
-        key={explorerData.id} 
-    />
+      <Folder
+        handleInsertNode={handleInsertNode}
+        handleDeleteNode={handleDeleteNode}
+        explorer={explorerData}
+      />
     </div>
   );
 }
