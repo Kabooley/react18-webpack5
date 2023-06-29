6f7a7a6782a60ae7fc070cdf14ccf5c3029996b7
Kabooley <35451559+Kabooley@users.noreply.github.com>
2023-06-29 23:19:26 +0900

    Update files.ts

--- src/data/files.ts
+++ src/data/files.ts
@@ -60,72 +60,108 @@ export const filesTemporary: iFile[] = [
 
 
 
-const filesProxy = (function(filesTemporary: iFile[]) {
-    // 参照を持たせないため
-    let _files: iFileWithId[] = filesTemporary.map((d, index) => {
-        return {...d, id: index};
+
+export const filesProxy = (function(filesTemporary: iFile[]) {
+  // 参照を持たせないため
+  let _files: iFileWithId[] = filesTemporary.map((d, index) => {
+      return {...d, id: index};
+  });
+
+  const _addFile = (newFile: iFile) => {
+      if(!_files.find(_f => _f.path === newFile.path)){
+          _files.push({
+              ...newFile, id: new Date().getTime()
+          });
+      }
+  };
+
+  const _getFile = (path: string) => {
+      const f = _files.find(_f => _f.path === path);
+      // 
+      // TODO: Make sure this way does return not a copy.
+      // 
+      return {
+          path: f?.path,
+          language: f?.language,
+          value: f?.value,
+          id: f?.id
+      };
+  };
+
+  const _getFiles = () => {
+    return _files.map(f => {
+      return {
+        path: f?.path,
+        language: f?.language,
+        value: f?.value,
+        id: f?.id
+      }
     });
+  };
 
-    const _addFile = (newFile: iFile) => {
-        if(!_files.find(_f => _f.path === newFile.path)){
-            _files.push({
-                ...newFile, id: new Date().getTime()
-            });
-        }
-    };
+  const _removeFile = (path: string) => {
+      if(_files.find(_f => _f.path === path)) {
+          _files = _files.filter(f => f.path !== path);
+      }
+  };
 
-    const _getFile = (path: string) => {
-        const f = _files.find(_f => _f.path === path);
-        // 
-        // TODO: Make sure this way does return not a copy.
-        // 
-        return {
-            path: f?.path,
-            language: f?.language,
-            value: f?.value,
-            id: f?.id
-        };
-    };
+  const _updateFile = (
+      id: number,
+      {path, language, value}
+      :{path?: string, language?: string, value?: string}) => {
+          let found = _files.find(_f => _f.id === id);
+          if(found) {
+              found.path = path !== undefined ? path : found.path;
+              found.language = language !== undefined ? language : found.language;
+              found.value = value !== undefined ? value : found.value;
+          }
+  };
 
-    const _removeFile = (path: string) => {
-        if(_files.find(_f => _f.path === path)) {
-            _files = _files.filter(f => f.path !== path);
-        }
-    };
-
-    const _updateFile = (
-        id: number,
-        {path, language, value}
-        :{path?: string, language?: string, value?: string}) => {
-            let found = _files.find(_f => _f.id === id);
-            if(found) {
-                found.path = path !== undefined ? path : found.path;
-                found.language = language !== undefined ? language : found.language;
-                found.value = value !== undefined ? value : found.value;
-            }
-    };
-
-
-    // Check if path is same
-    const _isFileAlreadyExist = (f: iFileWithId) => {
-        return _files.find(_f => _f.path === f.path) === undefined
-        ? true : false;
-    };
-
-    // ひとまずクリアする前に_filesをfileへ保存する。
-    const clearAll = () => {
-        filesTemporary = _files.map(f => f);
-        // TODO: うまい配列の消し方は？
-        _files = undefined;
-    }
-
-
-    return {
-        addFile: _addFile,
-        getFile: _getFile,
-        removeFile: _removeFile,
-        updateFile: _updateFile,
-    }
+  return {
+      addFile: _addFile,
+      getFile: _getFile,
+      getFiles: _getFiles,
+      removeFile: _removeFile,
+      updateFile: _updateFile,
+  }
 })(filesTemporary);
 
-// 要テスト
+// TEST: filesProxy
+// (function() {
+  // 検証１：getFilesで取得したfilesのコピーは参照を持つか？
+  // 結果：持たない。
+  // 
+  // const fff = filesProxy.getFiles();
+  // fff[0].path = "unjamaramme";
+  // console.log(filesProxy.getFiles());
+  // console.log(filesTemporary);
+
+  // 検証２：getFileで取得したfileは参照を持つか？
+  // 結果：持たない。
+  // 
+  // const aFile = filesProxy.getFile('/main.jsx');
+  // aFile.value = "drakengard";
+  // const fff = filesProxy.getFiles();
+  // console.log(fff);
+
+  // 検証3：addFileへ渡したオブジェクトを後から変更したら、_filesの中身も変更されてしまわないか？
+  // 結果：持たない。
+  // 
+  // const newFile = {
+  //   path: "/awesome.ts", language: "typescript", value: `const awesome = "awesome"`
+  // };
+  // filesProxy.addFile(newFile);
+  // newFile.path="aweeeohhhh";
+  // console.log(filesProxy.getFiles());
+
+  // 検証4：updateFileで渡したオブジェクトを後から変更したら、_filesの中身も変更されてしまわないか？
+  // 結果：持たない。
+  // 
+  // const awesomeOne = filesProxy.getFile("/awesome.ts");
+  // const awesome2= {
+  //   path: "/likealight.ts", language: "typescript", value: `const likealight = "likealight"`
+  // };
+  // filesProxy.updateFile(awesomeOne.id!, awesome2);
+  // awesome2.path = "astro";
+  // console.log(filesProxy.getFiles());
+// })();
