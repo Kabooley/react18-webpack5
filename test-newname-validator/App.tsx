4b511f5d08014ae36ae5c716cc0d89678b620a0c
Kabooley <35451559+Kabooley@users.noreply.github.com>
2023-07-18 02:56:50 +0900

    Update App.tsx

--- test-newname-validator/App.tsx
+++ test-newname-validator/App.tsx
@@ -1,28 +1,32 @@
 import "./styles.css";
-import type { iExplorer } from './Tree';
-import Tree from './Tree';
+import type { iExplorer } from "./Tree";
+import Tree from "./Tree";
 
 const explorerData: iExplorer = {
   id: "1",
   name: "root",
   isFolder: true,
+  path: "///",
   items: [
     {
       id: "2",
       name: "public",
       isFolder: true,
+      path: "///////",
       items: [
         {
           id: "5",
           name: "index.html",
           isFolder: false,
-          items: []
+          items: [],
+          path: "///////"
         },
         {
           id: "6",
           name: "style.css",
           isFolder: false,
-          items: []
+          items: [],
+          path: "///////"
         }
       ]
     },
@@ -30,44 +34,59 @@ const explorerData: iExplorer = {
       id: "3",
       name: "src",
       isFolder: true,
+      path: "///////",
       items: [
         {
           id: "7",
           name: "index.js",
           isFolder: false,
-          items: []
+          items: [],
+          path: "///////"
         },
         {
           id: "8",
           name: "react",
           isFolder: true,
+          path: "///////",
           items: [
             {
               id: "9",
               name: "index.jsx",
               isFolder: false,
-              items: []
-            },
+              items: [],
+              path: "///////"
+            }
           ]
-        },
-        
+        }
       ]
     },
     {
       id: "4",
       name: "assets",
       isFolder: true,
-      items: []
+      items: [],
+      path: "///////"
+    },
+    {
+      id: "99",
+      name: "temporary",
+      isFolder: true,
+      items: [],
+      path: "///////"
     }
   ]
 };
 
-
-
 export default function App() {
   return (
     <div className="App">
-      <Tree explorer={explorerData} nestDepth={0} />
+      <Tree
+        explorer={explorerData}
+        nestDepth={0}
+        handleDeleteNode={() => {}}
+        handleInsertNode={() => {}}
+        handleReorderNode={() => {}}
+      />
     </div>
   );
 }
